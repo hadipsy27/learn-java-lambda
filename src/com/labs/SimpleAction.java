a05f8e49de8f3a5fb61ce2f367f51b25aba89537
hadipsy27 <hadipsy27@gmail.com>
2023-07-28 08:52:31 +0700

    Lambda : Membuat Lambda

--- src/com/labs/SimpleAction.java
+++ src/com/labs/SimpleAction.java
@@ -3,5 +3,6 @@ package com.labs;
 @FunctionalInterface
 public interface SimpleAction {
 
-    String action();
+//    String action();
+    String action(String name);
 }
--- src/com/labs/app/SimpleActionApp.java
+++ src/com/labs/app/SimpleActionApp.java
@@ -6,21 +6,58 @@ public class SimpleActionApp {
     public static void main(String[] args) {
 
         // Anonymous class
+//        SimpleAction simpleAction1 = new SimpleAction() {
+//            @Override
+//            public String action() {
+//                return "Hadi";
+//            }
+//        };
+//
+//        System.out.println(simpleAction1.action());
+//
+//
+//        // contoh menggunakan lambda
+//        SimpleAction simpleAction2 = () -> {
+//            return "Prasetyo";
+//        };
+//
+//        System.out.println(simpleAction2.action());
+
+        // ------------------------------------------------------------------------------
+
         SimpleAction simpleAction1 = new SimpleAction() {
             @Override
-            public String action() {
-                return "Hadi";
+            public String action(String name) {
+                return "Hello " + name;
             }
         };
+        System.out.println(simpleAction1.action("Hadi"));
 
-        System.out.println(simpleAction1.action());
+        System.out.println("----------------------------------------------------------------");
 
-
-        // contoh menggunakan lambda
-        SimpleAction simpleAction2 = () -> {
-            return "Prasetyo";
+        SimpleAction simpleAction2 = (String value) -> {
+            return "Hello " + value;
         };
+        System.out.println(simpleAction2.action("Andi"));
 
-        System.out.println(simpleAction2.action());
+        System.out.println("----------------------------------------------------------------");
+        SimpleAction simpleAction3 = (value -> {
+            return "Hello " + value;
+        });
+        System.out.println(simpleAction3.action("Budi"));
+
+
+        System.out.println("----------------------------------------------------------------");
+        SimpleAction simpleAction4 = (String value) -> "Hello " + value;
+        System.out.println(simpleAction4.action("Dika"));
+
+        System.out.println("----------------------------------------------------------------");
+        SimpleAction simpleAction5 = (value) -> "Hello " + value;
+        System.out.println(simpleAction5.action("Angga"));
+
+        System.out.println("----------------------------------------------------------------");
+        SimpleAction simpleAction6 = value -> "Hello " + value;
+        System.out.println(simpleAction6.action("Joko"));
+
     }
 }
